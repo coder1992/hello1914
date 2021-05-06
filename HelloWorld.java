e6909e3f33ae4aba73a58ea45c537c28fbce0ad2
opensourse-test <83653979+opensourse-test@users.noreply.github.com>
2021-05-06 11:27:27 +0800

    Update HelloWorld.java
    this change is use to test PR

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package demo;
 
 public class HelloWorld {
 	public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("This chang is use to test PR!");
 	}
 }
